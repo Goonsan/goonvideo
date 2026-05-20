4d7fbf99a70d468320bdae0fffd4f712bd753bb1
Goonsan <goonsan.pro@gmail.com>
2026-05-20 13:13:57 +0200

    chore: add google analytics with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    partytown({
+      // Indispensable pour que Google Analytics transmette les données en arrière-plan
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
+});
